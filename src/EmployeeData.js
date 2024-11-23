b35c57e9f96a1b474a55e065678e06e6f3624f33
mdalamin-py <mdalaminislam.pro@gmail.com>
2024-11-23 20:47:01 +0600

    react simple crud project complete

---
+++ src/EmployeeData.js
@@ -0,0 +1,23 @@
+import React from "react";
+import App from "./App";
+
+export const EmployeeData = [
+    {
+        id: 1,
+        firstName: "Al Amin",
+        lastName: "Islam",
+        age: 20,
+    },
+    {
+        id: 2,
+        firstName: "Sabbir",
+        lastName: "Ahmed",
+        age: 23,
+    },
+    {
+        id: 3,
+        firstName: "Asfir",
+        lastName: "Noor",
+        age: 22,
+    },
+]
